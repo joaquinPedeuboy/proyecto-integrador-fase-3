49da331b28cfe9a52ff4aa99040fa3a5cc66979c
Joaquin Pedeuboy <joaquin.pedeuboy@hotmail.com>
2024-03-06 19:30:12 -0300

    Init project: Create STS Spring Boot project and initial configuration

---
+++ src/main/java/com/educacionit/ProyectoIntegradorFinalFase3Application.java
@@ -0,0 +1,13 @@
+package com.educacionit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoIntegradorFinalFase3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoIntegradorFinalFase3Application.class, args);
+	}
+
+}
