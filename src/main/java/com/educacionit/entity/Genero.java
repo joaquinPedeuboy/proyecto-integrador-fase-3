a3a474b462665b94e0fba9f05f7e9c331fdc98f7
Joaquin Pedeuboy <joaquin.pedeuboy@hotmail.com>
2024-03-08 16:18:09 -0300

    Mapping ORM and create database

--- src/main/java/com/educacionit/entity/Genero.java
+++ src/main/java/com/educacionit/entity/Genero.java
@@ -1,9 +1,27 @@
 package com.educacionit.entity;
 
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "generos")
 public class Genero {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "gen_id")
 	private Integer id;
+	@Column(name = "gen_nombre",nullable = false,length = 15)
 	private String nombreGenero;
+	@ManyToMany(mappedBy = "generos",fetch = FetchType.EAGER)
+	private Set<Pelicula> peliculas;
 }
--- src/main/java/com/educacionit/entity/ImagenPelicula.java
+++ src/main/java/com/educacionit/entity/ImagenPelicula.java
@@ -1,10 +1,28 @@
 package com.educacionit.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "imagenes")
 public class ImagenPelicula {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "img_id")
 	private Integer id;
+	@Column(name = "img_nombre_archivo",nullable = false,length = 100)
 	private String nombreArchivo;
+	@Lob
+	@Column(name = "img_imagen",nullable = false,columnDefinition = "LONGBLOB")
 	private byte[] imagen;
+	@OneToOne(mappedBy = "imagenPelicula")
+	private Pelicula pelicula;
 }
