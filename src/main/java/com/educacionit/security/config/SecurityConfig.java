5708777c854a2d11cc27e92862e753f0e31d759b
Joaquin Pedeuboy <joaquin.pedeuboy@hotmail.com>
2024-03-09 19:22:23 -0300

    Implements swagger

--- src/main/java/com/educacionit/security/config/SecurityConfig.java
+++ src/main/java/com/educacionit/security/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -27,18 +28,21 @@ public class SecurityConfig {
 	
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.csrf(csrf->csrf.disable())
-			.authorizeHttpRequests(auth->auth.requestMatchers(getPublicEndpoints()).permitAll()
-					.anyRequest().authenticated())
-			.sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-			.authenticationProvider(authenticationProvider)
-			.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+		http.authorizeHttpRequests(auth->auth.requestMatchers(getPublicEndpoints()).permitAll()
+				.anyRequest().authenticated())
+		.sessionManagement(sess->sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+		.authenticationProvider(authenticationProvider)
+		.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+		.csrf(csrf->csrf.disable());
 		return http.build();
 	}
 	
 	private RequestMatcher getPublicEndpoints() {
 		return new OrRequestMatcher(
-				new AntPathRequestMatcher("/api/auth/**"));
+				new AntPathRequestMatcher("/api/auth/**"),
+				new AntPathRequestMatcher("/swagger-ui/**"),
+		        new AntPathRequestMatcher("/v3/api-docs/**"),
+		        new AntPathRequestMatcher("/swagger-ui.html"));
 	}
 
 }
